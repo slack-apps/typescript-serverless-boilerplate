1fdd5d2a1973a0947f464511f1d57268c103c0a2
ysato <56924527+ysato@users.noreply.github.com>
2023-07-13 03:36:54 +0900

    feat: slash-commands & view

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -38,8 +38,66 @@ app.message('hello', async ({ message, say }) => {
   });
 });
 
+app.view('view_1', async ({ body, ack, view, client, logger }) => {
+  await ack();
+
+  const val = view.state.values['block_1']!['input_1']!['value'] as string;
+
+  try {
+    await client.chat.postMessage({
+      channel: view.private_metadata,
+      text: `<@${body.user.id}> submitted ${val}`,
+    });
+  } catch (error) {
+    logger.error(error);
+  }
+});
+
+app.command('/slash', async ({ logger, body, client, ack, say }) => {
+  await ack();
+
+  try {
+    const result = await client.views.open({
+      trigger_id: body.trigger_id,
+      view: {
+        private_metadata: body.channel_id,
+        type: 'modal',
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Modal title',
+        },
+        blocks: [
+          {
+            type: 'input',
+            block_id: 'block_1',
+            label: {
+              type: 'plain_text',
+              text: 'What are your hopes and dreams?',
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'input_1',
+            },
+          },
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit',
+        },
+      },
+    });
+    logger.info(result);
+  } catch (error) {
+    logger.error(error);
+  }
+
+  await say(`<@${body.user_id}> called /slash`);
+});
+
 app.action('button_click', async ({ body, ack, say }) => {
   await ack();
+
   await say(`<@${body.user.id}> clicked the button`);
 });
 
